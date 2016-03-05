1b723311a61c68026fd0606940805a90866cac82
Sven Piller <sven.piller@dlh.de>
2016-03-05 01:35:06 +0100

    Calls to Fraport API

--- backend/apicalls.js
+++ backend/apicalls.js
@@ -4,15 +4,34 @@ var https = require('https');
 
 var lh_api = {
   url: 'https://api-test.lufthansa.com/v1',
-  tokenurl: 'https://api-test.lufthansa.com/v1/oauth/token',
+  tokenurl: '/oauth/token',
   clientId: 'cx7bzwe8keuw264utrmhrayf',
   clientSecret: 'QvwRcfxnQB',
   token: null
 };
 
+var fraport_api = {
+  url: 'https://developer.fraport.de/api',
+//  tokenurl: '/v1/oauth/token',
+//  clientId: 'cfnGPUL92NfJigE8NasBowbB_U8a',
+//  clientSecret: 'n3HV6K3iR7aV5y9B0B7Cpn9AD5ga',
+  token: '9ddaee45bbf8d1bc26082b4116a7b484'
+};
+
 function initApis(callback) {
+  initLufthansaAPI(function () {
+    initFraportAPI(function () {
+      callback();
+    });
+  });
+
+}
+
+// Lufthansa stuff  ############################################################
+
+var initLufthansaAPI = function(callback) {
   request.post({
-    url: lh_api.tokenurl,
+    url: lh_api.url + lh_api.tokenurl,
     form: {
       'client_id':	lh_api.clientId,
       'client_secret':	lh_api.clientSecret,
@@ -24,13 +43,17 @@ function initApis(callback) {
     }
     if (err) console.log(err);
     var data = JSON.parse(body);
-    //console.log(data.access_token);
+    console.log('Lufthansa Token: ' + data.access_token);
     lh_api.token = data.access_token;
-    //console.log(lh_api);
     callback();
   });
 }
 
+/**
+* @param {string} endpoint the route in the api
+* @param {string} data the filter data for the query
+* @param {string} callback callback(err, response)
+*/
 var performLufthansaRequest = function(endpoint, data, callback) {
   request.get({
     url: lh_api.url + '/' + endpoint + '?' + querystring.stringify(data),
@@ -42,10 +65,66 @@ var performLufthansaRequest = function(endpoint, data, callback) {
     if(httpResponse.statusCode !== 200){
       return console.log('2: Invalid Status Code Returned:', httpResponse.statusCode);
     }
+    if (err) {
+      console.log(err);
+    }
+    if (callback) {
+      callback(JSON.parse(body));
+    }
+  });
+}
+
+// Fraport Stuff ###############################################################
+
+var initFraportAPI = function(callback) {
+  // Not necessary, FRAPORT-Token lasts for 40 hours
+  console.log('Fraport Token: ' + fraport_api.token);
+  callback();
+  /*
+  request.post({
+    url: fraport_api.url + fraport_api.tokenurl,
+    form: {
+      'client_id':	fraport_api.clientId,
+      'client_secret':	fraport_api.clientSecret,
+      'grant_type':	"client_credentials"
+    }
+  }, function(err, httpResponse, body){
+    if(httpResponse.statusCode !== 200){
+      return console.log('3: Invalid Status Code Returned:', httpResponse.statusCode);
+    }
     if (err) console.log(err);
-    //console.log(body);
-    // data = JSON.parse(body);
-    //console.log(data);
+    var data = JSON.parse(body);
+    console.log('Fraport Token: ' + data.access_token);
+    fraport_api.token = data.access_token;
+    callback();
+  }); */
+}
+
+/**
+* @param {string} api the actual api to query (ex. flights)
+* @param {string} endpoint the route in the api (ex. /flight/FRA/arrival)
+* @param {string} (optional) data the filter data for the query
+* @param {string} callback callback(err, response)
+*/
+var performFraportRequest = function(api, endpoint, data, callback) {
+  var url = fraport_api.url + '/' + api + '/1.0' + endpoint;
+  if (data){
+    url += '?' + querystring.stringify(data);
+  }
+  //console.log(url);
+  request.get({
+    url:  url ,
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + fraport_api.token
+    }
+  }, function(err, httpResponse, body){
+    if(httpResponse.statusCode !== 200){
+      return console.log('4: Invalid Status Code Returned:', httpResponse.statusCode);
+    }
+    if (err) {
+      console.log(err);
+    }
     if (callback) {
       callback(JSON.parse(body));
     }
@@ -54,3 +133,4 @@ var performLufthansaRequest = function(endpoint, data, callback) {
 
 exports.initApis = initApis;
 exports.performLufthansaRequest = performLufthansaRequest;
+exports.performFraportRequest = performFraportRequest;
